9f9cdd8bd852c4f79f71bf4226c2e93cf58d2a00
Xai Hang <xaihang12@gmail.com>
2022-12-12 11:17:17 -0600

    q4g favoriteFoods converted to string and separated with the word "and"

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -101,6 +101,8 @@ console.log('Reserve food order:', favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+favoriteFoods.toString();
+console.log('Converted from array to string:', favoriteFoods.join(', and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
