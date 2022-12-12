491ebc88408287d25e20a4729159f8aeead054ed
Xai Hang <xaihang12@gmail.com>
2022-12-12 11:01:05 -0600

    q4e removed current/replaced with new food, updated array with new replacement

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -88,6 +88,10 @@ console.log('The foods are now', favoriteFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+secondFood = favoriteFoods.splice(1, 1, 'pasta'); 
+console.log('Removed the second food', secondFood); 
+console.log('The foods are now', favoriteFoods); 
+
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
