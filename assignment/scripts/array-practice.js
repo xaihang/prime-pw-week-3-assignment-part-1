3deabfc003a5c8c96db10652a750884f2469c6a1
Xai Hang <xaihang12@gmail.com>
2022-12-12 10:22:58 -0600

    q3abc console 2nd and 3rd animal along with stretch goal

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -35,12 +35,12 @@ console.log('First animal is', animalArray[0])
 console.log('Second animal is', animalArray[1]); 
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+console.log('Last animal is', animalArray[3]); 
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
-
+let lastAnimal = animalArray[animalArray.length - 1];
+console.log('Last animal is', lastAnimal);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
