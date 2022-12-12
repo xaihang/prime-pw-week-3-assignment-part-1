a083a987dc8eb8689cc73433bd5c8f38676c9c94
Xai Hang <xaihang12@gmail.com>
2022-12-12 10:42:29 -0600

    q4b used pop method, removed last food item from array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -61,6 +61,9 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
+let removedFood = favoriteFoods.pop();
+console.log('Removed the last food', removedFood);
+console.log('The foods are now', favoriteFoods); 
 
 
 // Example: Add an animal to the beginning using Array.unshift
